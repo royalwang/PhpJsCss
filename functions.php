7a64ddae280d0bec2b864ec82553dcdd1c3f0d1c
fxstar.eu <hello@fxstar.eu>
2016-03-26 10:05:54 +0100

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,6 +4,26 @@ header('Thu, 01 Jan 1970 00:00:00 GMT');
 session_start();
 error_reporting(0);
 
+function curl_get_contents($url) {
+       // Initiate the curl session
+ $ch = curl_init();
+      // Set the URL
+ curl_setopt($ch, CURLOPT_URL, $url);
+     // Removes the headers from the output
+ curl_setopt($ch, CURLOPT_HEADER, 0);
+     // Return the output instead of displaying it directly
+ curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // Execute the curl session
+ $output = curl_exec($ch);
+    // Close the curl session
+ curl_close($ch);
+    // Return the output as a variable
+ return $output;
+}
+// use 
+//echo $output = curl_get_contents('http://fxstar.eu/');
+
+
 // time from mysql timestamp
 // echo date('M j Y g:i A', strtotime('2010-05-29 01:17:35'));
 $dayofweek = date('w', strtotime('2016-02-1'));
