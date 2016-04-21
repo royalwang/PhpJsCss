2853d9d932dbc413f59b0bfd810cbf16e2325f98
fxstar.eu <hello@fxstar.eu>
2016-04-21 09:43:22 +0200

    Create Yahoo-Charts.js

---
+++ Yahoo-Charts.js
@@ -0,0 +1,7 @@
+// get charts from yahoo finance and show image
+
+// day
+http://chart.finance.yahoo.com/z?s=GBPJPY=X&t=1d&q=l&l=on&z=s&p=m50,m200
+
+// month
+http://chart.finance.yahoo.com/z?s=GBPJPY=X&t=1m&q=l&l=on&z=s&p=m50,m200
