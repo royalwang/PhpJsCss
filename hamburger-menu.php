77a422242d5328edd38bf32be77c931cbcac7f3d
fxstar.eu <hello@fxstar.eu>
2016-05-24 20:59:55 +0200

    Update hamburger-menu.php

--- hamburger-menu.php
+++ hamburger-menu.php
@@ -3,3 +3,35 @@
 
 <!-- animated css menu button -->
 https://jonsuh.com/hamburgers/
+
+<!-- html style -->
+<div id="top" style="margin-bottom: 5px;">
+<style type="text/css">
+#lines {
+    border-bottom: 13px double #ff2233;
+    border-top: 4px solid #ff2233;
+    content: "";
+    height: 5px;
+    width: 30px;
+}
+.menubtn {
+    position: relative;
+    float: right;
+    top: 5px;
+    right: 20px;
+    max-width: 30px;
+    cursor: pointer;
+    margin-left: 20px;
+}
+.menubtn {
+    position: relative;
+    float: right;
+    top: 5px;
+    right: 20px;
+    max-width: 30px;
+    cursor: pointer;
+}
+</style>
+	<div id="lines" class="menubtn"></div>
+	<!-- <a href="index.php" style="padding: 0px; margin: 0px;"><img src="img/logo.jpg" class="logo"> </a> -->
+</div>
