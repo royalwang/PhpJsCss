7bd075c42d00a4466ad15aa330b4b68d03db0edf
fxstar.eu <hello@fxstar.eu>
2016-05-25 09:05:01 +0200

    Update hamburger-menu.php

--- hamburger-menu.php
+++ hamburger-menu.php
@@ -1,3 +1,6 @@
+<!-- use icon fonts like this -->
+http://fontawesome.io/icon/bars/
+
 <!-- unicode font menu button -->
 <a href="#menu" style="color: #ff2233; font-size: 23px;">  &#9776; Menu </a>
 
@@ -13,15 +16,6 @@ https://jonsuh.com/hamburgers/
     content: "";
     height: 5px;
     width: 30px;
-}
-.menubtn {
-    position: relative;
-    float: right;
-    top: 5px;
-    right: 20px;
-    max-width: 30px;
-    cursor: pointer;
-    margin-left: 20px;
 }
 .menubtn {
     position: relative;
