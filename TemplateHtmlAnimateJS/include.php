afb0cafd17705f0b743adc2d96cd6307c3d50c91
fxstar.eu <hello@fxstar.eu>
2016-06-21 15:37:58 +0200

    Create include.php

---
+++ TemplateHtmlAnimateJS/include.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<title>test</title>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">  
+	<meta name="description" content="Like button">
+    <meta name="keywords" content="Like button" />
+    <!-- open links in new tab -->
+    <base target="_blank" />
+</head>
+<style type="text/css">
+*{
+	text-decoration: none;
+	font-family: Arial;
+	font-size: 12px;
+}
+.hi_like{
+background: #fff;
+}
+label{
+	padding: 3px;
+	border-radius: 10px;
+}
+</style>
+<body style="background: none; padding: 5px;">
+<div>
+<a href="https://fxstar.eu" title="Polubienia @fxstareu" id="hi_link" style="color: #000;"> Polubienia 123456</a>
+</div>
+</body>
+</html>
