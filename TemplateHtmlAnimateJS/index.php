a7c9b3946aa6f4452b74caec90faa94dfd6d6200
fxstar.eu <hello@fxstar.eu>
2016-06-21 14:51:14 +0200

    Update index.php

--- TemplateHtmlAnimateJS/index.php
+++ TemplateHtmlAnimateJS/index.php
@@ -8,7 +8,7 @@
 ?>
 <html lang="pl">
 <head>
-	<title>test</title>
+	<title>TemplateHtmlAnimateJS</title>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">  
 	<meta name="description" content="">
