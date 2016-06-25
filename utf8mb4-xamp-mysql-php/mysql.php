b29b0a4454e63e9d1901c5f67b3938b95d7aabc8
fxstar.eu <hello@fxstar.eu>
2016-06-25 13:30:08 +0200

    Update mysql.php

--- utf8mb4-xamp-mysql-php/mysql.php
+++ utf8mb4-xamp-mysql-php/mysql.php
@@ -22,9 +22,9 @@ return $connection;
 $db = Conn();
 
 // GET POST DATA
-$u = htmlentities($_POST['user'], ENT_QUOTES, 'utf-8');
-$e = htmlentities($_POST['email'], ENT_QUOTES, 'utf-8');
-$p = htmlentities($_POST['pass'], ENT_QUOTES, 'utf-8');
+$u = htmlentities($_POST['user'], ENT_QUOTES, 'UTF-8');
+$e = htmlentities($_POST['email'], ENT_QUOTES, 'UTF-8');
+$p = htmlentities($_POST['pass'], ENT_QUOTES, 'UTF-8');
 
 
 // INSERT TO MYSQL
