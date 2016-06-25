9bde80f9ac0267c15a18b50f6353c3f89e9921b5
fxstar.eu <hello@fxstar.eu>
2016-06-25 12:56:55 +0200

    Update mysql-show-variables.sql

--- utf8mb4-xamp-mysql-php/mysql-show-variables.sql
+++ utf8mb4-xamp-mysql-php/mysql-show-variables.sql
@@ -31,3 +31,20 @@ collation_connection 	utf8mb4_unicode_ci
 collation_database 	utf8_general_ci
 collation_server 	latin1_swedish_ci
 */
+
+# after change looks (_ci - case insensitive, size of characters dosen't matter when search and insert data):
+# SHOW VARIABLES WHERE Variable_name LIKE 'character\_set\_%' OR Variable_name LIKE 'collation%';
+
+/*
+character_set_client 	utf8mb4
+character_set_connection 	utf8mb4
+character_set_database 	utf8mb4
+character_set_filesystem 	binary
+character_set_results 	utf8mb4
+character_set_server 	utf8mb4
+## utf8 here is ok
+character_set_system 	utf8
+collation_connection 	utf8mb4_unicode_ci
+collation_database 	utf8mb4_unicode_ci
+collation_server 	utf8mb4_unicode_ci
+*/
