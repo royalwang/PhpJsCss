d681c45dd7588b4ed46dfd5fe0cda99c31063c5d
fxstar.eu <hello@fxstar.eu>
2016-07-11 19:32:50 +0200

    Create secureshowsession.php

---
+++ SecureFolderFiles/secureshowsession.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+error_reporting(0);
+if ($_SESSION['ok'] != 1) {
+  header('Location: login.php');
+}
+$id = (int)$_SESSION['idsecret'];
+
+$f = "folder/".$id.'user.html';
+$type = mime_content_type($f);
+
+	if (!file_exists($f)) {
+	header('Content-Type: text/html; charset=utf-8');
+	header('HTTP/1.1 403 Forbidden');
+	echo "Nie masz dostępu do tej zawartości";
+	exit();		
+	}
+
+if ($type == 'text/html' || $type == 'text/plain') {
+	header("Content-type: ".$type."; ".'charset=utf-8');
+	echo file_get_contents($f);		
+	exit();
+}else{
+	header('Content-Type: text/html; charset=utf-8');
+	header('HTTP/1.1 403 Forbidden');
+	echo "Nie masz dostępu do tej zawartości";
+	exit();
+}
