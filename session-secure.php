bb53b59598cfb5e704a54c37b6911eaeafb744e2
fxstar.eu <hello@fxstar.eu>
2016-07-15 11:12:26 +0200

    Update session-secure.php

--- session-secure.php
+++ session-secure.php
@@ -74,14 +74,43 @@ echo "POST username " . $username = htmlentities($_POST['username'], ENT_QUOTES,
 
 // when click on form button
 if(isset($_POST['submit'])){
-// Username from form ( . <- concat text and variable in one string)
-echo "Username from form " . $_POST['username'] . "<br>";
-
-// print array with file(s) to upload
-echo "Files from form: <br>";
-print_r($_FILES);
+    // Username from form ( . <- concat text and variable in one string)
+    echo "Username from form " . $_POST['username'] . "<br>";
+    
+    // print array with file(s) to upload
+    echo "Files from form (only images allowed): <br>";
+    print_r($_FILES);
+    
+    // single file upload
+    $uploads_dir = 'uploads2';
+    // create folder
+    mkdir($uploads_dir, 0755, true);    
+    $name = $_FILES["file"]["name"];
+    move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], "$uploads_dir/".$name);
+    // end single file upload
+    
+    // AND multiple file upload
+    $uploads_dir = 'uploads1';
+    // create folder
+    mkdir($uploads_dir, 0755, true);
+    // allowed file extensions only images
+    $allowed = array('gif','jpg','jpeg','png');
+    // upload files
+    foreach ($_FILES["file"]["error"] as $key => $error) {
+        if ($error == UPLOAD_ERR_OK) {
+            $tmp_name = $_FILES["file"]["tmp_name"][$key];
+            $name = $_FILES["file"]["name"][$key];
+            $ext = pathinfo($name, PATHINFO_EXTENSION);
+            // check files extensions
+            if (in_array($ext, $allowed)) {              
+              // upload file to server folder
+              move_uploaded_file($tmp_name, "$uploads_dir/".$name);
+            }
+        }
+    }
+    // end multiple file upload
+  
 }
-
 ?>
 
 <form method="post" action="" enctype="multipart/form-data">
@@ -96,3 +125,52 @@ print_r($_FILES);
   <!-- submit button -->
   <input type="submit" name="submit" value="Send POST">
 </form>
+
+<?php
+// clear POST and GET input 
+function Clear(){
+  foreach ($_GET as $key => $val) { 
+      if (is_string($val)) { 
+          $_GET[$key] = htmlentities($val, ENT_QUOTES, 'UTF-8'); 
+      } else if (is_array($val)) { 
+          $_GET[$key] = Clear($val); 
+      } 
+  } 
+  foreach ($_POST as $key => $val) { 
+      if (is_string($val)) { 
+          $_POST[$key] = htmlentities($val, ENT_QUOTES, 'UTF-8'); 
+      } else if (is_array($val)) { 
+          $_POST[$key] = Clear($val); 
+      } 
+  } 
+}
+// use
+Clear();
+
+// PDO mysql connection
+function Conn(){
+$connection = new PDO('mysql:host=localhost;dbname=xmail;mysql:charset=utf8mb4', 'root', 'toor');
+$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$connection->setAttribute(PDO::ATTR_PERSISTENT, true);
+$connection->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8mb4' COLLATE 'utf8mb4_unicode_ci'");
+return $connection;
+}
+
+// strip java script tags from html content
+function strip_javascript($filter, $allowed=1){
+if($allowed == 1) // 1 href=...
+$filter = preg_replace('/href=([\'"]).*?javascript:.*?\\1/i', "'", $filter);
+
+if($allowed == 1) // 2 <script....
+$filter = preg_replace("/<script.*?>.*?<\/script>/i", "", $filter);
+
+if($allowed == 1) // 4 <tag on.... ---- useful for onlick or onload
+$filter = preg_replace("/<(.*)?\son.+?=\s*?(['\"]).*?\\2/i", "<$1", $filter);
+return $filter;
+}
+
+?>
+
+
+
