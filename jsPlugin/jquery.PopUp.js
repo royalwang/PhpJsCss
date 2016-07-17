a0eb4ab0fa857678293b5ba75659a1f4efb755bd
fxstar.eu <hello@fxstar.eu>
2016-07-17 14:34:29 +0200

    Update jquery.PopUp.js

--- jsPlugin/jquery.PopUp.js
+++ jsPlugin/jquery.PopUp.js
@@ -7,10 +7,10 @@ $("body").PopUp("Info text");
 $('.popup').delay(2000).fadeOut(300);
 </script>
 */
-jQuery.fn.PopUp = function (alert = 'Hello my friends ...', err = 0) {	
-	var popup = '<div class="popup" style="position: absolute;  left: 50%;  top: 50%;  transform: translate(-50%, -100%);"><p class="alert alert-success">'+alert+'</p></div>'
+jQuery.fn.PopUp = function (alert = 'Kontakt został dodany', err = 1) {	
+	var popup = '<div class="popup" style="position: fixed;  left: 50%;  top: 50%;  transform: translate(-50%, -100%);"><p class="alert alert-success">'+alert+'</p></div>'
     if (err == 1) {
-        var popup = '<div class="popup" style="position: absolute;  left: 50%;  top: 50%;  transform: translate(-50%, -100%);"><p class="alert alert-error">'+alert+'</p></div>'
+        var popup = '<div class="popup" style="position: fixed;  left: 50%;  top: 50%;  transform: translate(-50%, -100%);"><p class="alert alert-error">'+alert+'</p></div>'
     }
 	this.append(popup);
     return this;
