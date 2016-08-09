688542f1513b50970cc355612741c335ae87255d
fxstar.eu <hello@fxstar.eu>
2016-08-09 09:24:03 +0200

    Update _functions.php

--- Default/core/_functions.php
+++ Default/core/_functions.php
@@ -42,6 +42,46 @@ $connection->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8mb4' COL
 return $connection;
 }
 
+//resize image
+function resizeImage($file = 'image.png', $maxwidth = 1366){
+  error_reporting(0);  
+  $image_info = getimagesize($file);
+  $image_width = $image_info[0];
+  $image_height = $image_info[1];
+  $ratio = $image_width / $maxwidth;
+  $info = getimagesize($file);
+  if ($image_width > $maxwidth) {
+    // GoGoGo
+    $newwidth = $maxwidth;
+    $newheight = (int)($image_height / $ratio);
+      
+    if ($info['mime'] == 'image/jpg') {    
+      $thumb = imagecreatetruecolor($newwidth, $newheight);
+      $source = imagecreatefrompng($file);
+      imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $image_width, $image_height);
+      imagejpeg($thumb,$file,90);
+    }
+    
+    if ($info['mime'] == 'image/png') {
+      echo "PNG";
+      $im = imagecreatefrompng($file);
+      $im_dest = imagecreatetruecolor($newwidth, $newheight);
+      imagealphablending($im_dest, false);
+      imagecopyresampled($im_dest, $im, 0, 0, 0, 0, $newwidth, $newheight, $image_width, $image_height);
+      imagesavealpha($im_dest, true);
+      imagepng($im_dest, $file, 9);
+    }
+    if ($info['mime'] == 'image/gif') {
+      $im = imagecreatefromgif($file);
+      $im_dest = imagecreatetruecolor($newwidth, $newheight);
+      imagealphablending($im_dest, false);
+      imagecopyresampled($im_dest, $im, 0, 0, 0, 0, $newwidth, $newheight, $image_width, $image_height);
+      imagesavealpha($im_dest, true);
+      imagegif($im_dest, $file);
+    }
+  }
+}
+
 // log database
 function logDB(){
     // create database first
