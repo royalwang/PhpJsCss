630b8a0d695af47ab54d21810442876691c6fa32
fxstar.eu <hello@fxstar.eu>
2016-09-13 19:51:47 +0200

    Update email_on_page.php

--- email_on_page.php
+++ email_on_page.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting('E_ALL');
 
-// emaile
+// emaile po jednym w linii
 $in = 'email.txt';
 // plik 2
 $out = 'emailall.txt';
@@ -10,17 +10,19 @@ file_put_contents($out, '');
 // strona www
 $html = file_get_contents('https://fxstar.eu');
 
+// otwórz plik
 $handle = @fopen($in, "r");
 if ($handle) {
+   // pobieraj linijka po linijce
     while (($buffer = fgets($handle, 4096)) !== false) {    	
-        //Zapisz email do pliku jeżeli poprawny
-		if (!filter_var(trim($buffer), FILTER_VALIDATE_EMAIL) === false) {
-				// wyświetl email i do nowej linj w przeglądarce
-				if (strpos($html, trim($buffer)) !== false) {
-				    echo 'Istnieje na stronie ' . $buffer."<br>";
-				    //dodaj do pliku emailall.txt
-        			file_put_contents($out, $buffer, FILE_APPEND | LOCK_EX);
-				}
+        //jeżeli adres email poprawny
+	if (!filter_var(trim($buffer), FILTER_VALIDATE_EMAIL) === false) {
+		// wyświetl email i do nowej linj w przeglądarce
+		if (strpos($html, trim($buffer)) !== false) {
+		    echo 'Istnieje na stronie ' . $buffer."<br>";
+		    //dodaj do pliku emailall.txt
+		    file_put_contents($out, $buffer, FILE_APPEND | LOCK_EX);
+		}
         }        
     }
     if (!feof($handle)) {
