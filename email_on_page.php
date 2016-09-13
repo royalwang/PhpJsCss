c19fc4cebd992cb4b9ba1da3c23033db13a77582
fxstar.eu <hello@fxstar.eu>
2016-09-13 18:49:17 +0200

    Create email_on_page.php

---
+++ email_on_page.php
@@ -0,0 +1,31 @@
+<?php
+error_reporting('E_ALL');
+
+// emaile
+$in = 'email.txt';
+// plik 2
+$out = 'emailall.txt';
+file_put_contents($out, '');
+
+// strona www
+$html = file_get_contents('https://fxstar.eu');
+
+$handle = @fopen($in, "r");
+if ($handle) {
+    while (($buffer = fgets($handle, 4096)) !== false) {    	
+        //Zapisz email do pliku jeżeli poprawny
+		if (!filter_var(trim($buffer), FILTER_VALIDATE_EMAIL) === false) {
+				// wyświetl email i do nowej linj w przeglądarce
+				if (strpos($html, trim($buffer)) !== false) {
+				    echo 'Istnieje na stronie ' . $buffer."<br>";
+				    //dodaj do pliku emailall.txt
+        			file_put_contents($out, $buffer, FILE_APPEND | LOCK_EX);
+				}
+        }        
+    }
+    if (!feof($handle)) {
+        echo "Error: unexpected fgets() fail\n";
+    }
+    fclose($handle);
+}
+?>
