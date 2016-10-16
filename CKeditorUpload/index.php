a91bfe99baf04b510a47935ef838a3f92212030a
fxstar.eu <hello@fxstar.eu>
2016-10-16 18:22:04 +0200

    Update index.php

--- CKeditorUpload/index.php
+++ CKeditorUpload/index.php
@@ -15,3 +15,30 @@
         </script>
     </body>
 </html>
+
+<!-- OR this -->
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CKEditor</title>
+        <script src="ckeditor.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    </head>
+    <body>
+        <textarea name="editor1"></textarea>
+        <script>        
+        $(function(){
+            $("html").on( "click", function() {
+                var data = CKEDITOR.instances.editor1.getData();              
+            });
+        });
+    
+        CKEDITOR.replace( 'editor1' , {
+            filebrowserBrowseUrl: 'browse.php?type=Files',
+            filebrowserUploadUrl: 'upload.php?type=Files',
+        });     
+        </script>
+    </body>
+</html>
