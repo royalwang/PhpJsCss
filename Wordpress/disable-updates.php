a8998ad71423744323803e95076cfc100ac62c0e
fxstar.eu <hello@fxstar.eu>
2017-01-14 14:33:19 +0100

    Wordpress disable all updates function.php
    Wordpress disable all updates function.php

---
+++ Wordpress/disable-updates.php
@@ -0,0 +1,10 @@
+<?php  
+// Add this to functions.php
+
+// error reporting 0 or "E_ALL"
+error_reporting(0);
+// Wordpress disable all updates.
+add_filter( 'allow_dev_auto_core_updates', '__return_false' );
+add_filter( 'auto_update_plugin', '__return_false' );
+add_filter( 'auto_update_theme', '__return_false' );
+?>
