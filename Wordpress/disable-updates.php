13ff3a56d4a5e79e723c6d984a365c6431ecc225
fxstar.eu <hello@fxstar.eu>
2017-01-16 11:23:05 +0100

    Wp disable all updates

--- Wordpress/disable-updates.php
+++ Wordpress/disable-updates.php
@@ -4,10 +4,9 @@
 // error reporting 0 or "E_ALL"
 error_reporting(0);
 // Wordpress disable all updates.
+add_filter( 'automatic_updater_disabled', '__return_true' );
+// or this
 add_filter( 'allow_dev_auto_core_updates', '__return_false' );
 add_filter( 'auto_update_plugin', '__return_false' );
 add_filter( 'auto_update_theme', '__return_false' );
-
-// disable all updates
-add_filter( 'automatic_updater_disabled', '__return_true' );
 ?>
