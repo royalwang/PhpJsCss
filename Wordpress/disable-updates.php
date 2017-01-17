3d3c0ef8f0efdd76d6e7b5d260e2b63301beff43
fxstar.eu <hello@fxstar.eu>
2017-01-17 19:11:08 +0100

    Update disable-updates.php

--- Wordpress/disable-updates.php
+++ Wordpress/disable-updates.php
@@ -9,4 +9,7 @@ add_filter( 'automatic_updater_disabled', '__return_true' );
 add_filter( 'allow_dev_auto_core_updates', '__return_false' );
 add_filter( 'auto_update_plugin', '__return_false' );
 add_filter( 'auto_update_theme', '__return_false' );
+
+// show admin top toolbar in user mode
+add_filter('show_admin_bar', '__return_true');
 ?>
