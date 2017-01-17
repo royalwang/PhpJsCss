63833e05671dda0e945148b35aa89cd62832c866
fxstar.eu <hello@fxstar.eu>
2017-01-17 20:56:24 +0100

    Update disable-updates.php

--- Wordpress/disable-updates.php
+++ Wordpress/disable-updates.php
@@ -11,5 +11,11 @@ add_filter( 'auto_update_plugin', '__return_false' );
 add_filter( 'auto_update_theme', '__return_false' );
 
 // show admin top toolbar in user mode
-add_filter('show_admin_bar', '__return_true');
+// add_filter('show_admin_bar', '__return_true');
+
+// Allow admin panel for subscriber with Woocommerce
+add_filter('woocommerce_disable_admin_bar', '_wc_disable_admin_bar', 10, 1); 
+function _wc_disable_admin_bar($prevent_admin_access) { return false;} 
+add_filter('woocommerce_prevent_admin_access', '_wc_prevent_admin_access', 10, 1); 
+function _wc_prevent_admin_access($prevent_admin_access) { return false; }
 ?>
