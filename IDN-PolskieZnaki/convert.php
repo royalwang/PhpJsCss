88210f79a18d3b22b62ae64c3fc5c4afaf94f226
Fxstar.eu <hello@fxstar.eu>
2017-02-05 20:03:48 +0100

    IND Polskie znaki url konwersja
    IND Polskie znaki url konwersja

--- IDN-PolskieZnaki/convert.php
+++ IDN-PolskieZnaki/convert.php
@@ -1,7 +1,8 @@
 <?php
 /// Show here
 /// https://github.com/phpWhois/idna-convert
-
+// lub
+// https://github.com/fxstar/idna-convert
 header('Content-Type: text/html; charset=utf-8');
 require('idna_convert.class.php');
 $idn_version = isset($_REQUEST['idn_version']) && $_REQUEST['idn_version'] == 2003 ? 2003 : 2008;
