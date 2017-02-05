c8c951a7d993fdbd5f195523126c1e597bdf2496
Fxstar.eu <hello@fxstar.eu>
2017-02-05 20:02:27 +0100

    IDN Polskie znaki url, rewrite convert
    IDN Polskie znaki url, rewrite convert, konwersja idn na utf-8

---
+++ IDN-PolskieZnaki/convert.php
@@ -0,0 +1,12 @@
+<?php
+/// Show here
+/// https://github.com/phpWhois/idna-convert
+
+header('Content-Type: text/html; charset=utf-8');
+require('idna_convert.class.php');
+$idn_version = isset($_REQUEST['idn_version']) && $_REQUEST['idn_version'] == 2003 ? 2003 : 2008;
+$IDN = new idna_convert(array('idn_version' => $idn_version));
+$city = explode('.', urldecode($_SERVER['HTTP_HOST']))[0];
+//$input = utf8_encode($_SERVER['HTTP_HOST']);
+$city = ucfirst(explode('.', $IDN->decode($_SERVER['HTTP_HOST']))[0]);
+?>
