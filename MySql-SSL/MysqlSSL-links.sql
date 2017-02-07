c6a6ebc95e700691065176526b4b9f5327c5c4bb
Fxstar.eu <hello@fxstar.eu>
2017-02-07 09:59:15 +0100

    Links

---
+++ MySql-SSL/MysqlSSL-links.sql
@@ -0,0 +1,20 @@
+Hello you need download this files:
+
+## 1) Download and Install Mysql (32 or 64 bit)
+https://dev.mysql.com/downloads/installer/
+
+## 2) Install OpenSSL from (32 or 64 bit)
+http://www.slproweb.com/products/Win32OpenSSL.html
+help here
+https://dev.mysql.com/doc/refman/5.5/en/creating-ssl-files-using-openssl.html
+
+## 3) Install Visual C++ Redistributable Packages for Visual Studio 2013
+https://dev.mysql.com/resources/wb62_prerequisites.html
+http://www.microsoft.com/en-us/download/details.aspx?id=40784
+
+## 4) Download and Install Mysql Workbench (32 or 64 bit)
+https://www.mysql.com/products/workbench/
+
+## 5) Create ssl certs from workbench ssl wizard
+
+## 6) Add certs to my.ini
