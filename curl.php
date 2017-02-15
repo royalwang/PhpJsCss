604f225c2f96162a777e2624bc2a66e2669a40f4
Fxstar.eu <hello@fxstar.eu>
2017-02-15 08:48:14 +0100

    Update curl.php

--- curl.php
+++ curl.php
@@ -24,13 +24,13 @@ echo $output = curl_get_contents('http://fxstar.eu/');
 <?php
 // SEND CURL POST DATA
  $jsonData = array(
-    'username' => 'MyUsername',
-    'password' => 'MyPassword'
+    'user' => 'Username',
+    'pass' => 'Password'
 ); 
 //Encode the array into JSON.
 $jsonDataEncoded = json_encode($jsonData);
 //API Url
-$url = 'http://example.com/api/JSON/create';
+$url = 'http://fxstar.eu/JSON/api.php';
  //Initiate cURL.
 $ch = curl_init($url);
 //Tell cURL that we want to send a POST request.
@@ -41,4 +41,10 @@ curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  
 //Execute the request
 $result = curl_exec($ch);
+?>
+
+<?php
+// GET JSON CONTENT FROM JS api.php
+$jsonStr = file_get_contents("php://input"); //read the HTTP body.
+$json = json_decode($jsonStr);
 ?>
