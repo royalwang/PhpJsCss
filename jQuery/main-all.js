a916e9dbc009d3a0ac43561987f27e246cc5a6bc
Fxstar.eu <hello@fxstar.eu>
2017-02-27 11:11:46 +0100

    Create main-all.js

---
+++ jQuery/main-all.js
@@ -0,0 +1,233 @@
+$(document).ready(function(){
+
+/* particlesJS enable uncoment - yuo can setup js/particles.json */
+particlesJS.load('particles-js', 'js/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+// knob circle animation
+$('.knob').each(function () {
+   var $this = $(this);
+   var myVal = $this.attr("rel");
+   // alert(myVal);
+   $this.knob({
+    readOnly: true,
+        //fgColor: '#339933',
+        //bgColor: '#eee'
+   });
+   $({
+       value: 0
+   }).animate({
+       value: myVal
+   }, {
+       duration: 2000,
+       easing: 'swing',
+       step: function () {
+           $this.val(Math.ceil(this.value)).trigger('change');
+       }
+   })
+});
+
+//Animate when visible
+jQuery('.col4').one('inview', function (event, visible) {
+    if (visible == true) {
+        jQuery(this).addClass("animated fadeInDown");
+    } else {
+        //jQuery(this).removeClass("animated fadeInDown");
+    }
+});
+
+
+jQuery('.fa').bind('inview', function (event, visible) {
+    if (visible == true) {
+        jQuery(this).addClass("animated fadeInUp");
+    } else {
+        //jQuery(this).removeClass("animated fadeInUp");
+    }
+});
+
+
+jQuery('.col3').bind('inview', function (event, visible) {
+    if (visible == true) {
+        jQuery(this).addClass("animated swing");
+    } else {
+        //jQuery(this).removeClass("animated swing");
+    }
+});
+
+jQuery('.col5').one('inview', function (event, visible) {
+    if (visible == true) {
+        jQuery(this).addClass("animated flipInX");
+    } else {
+        //jQuery(this).removeClass("animated fadeInDown");
+    }
+});
+
+/* Scroll to class, div */
+$("#button").click(function() {
+$('html, body').animate({
+    scrollTop: $("#target-element").offset().top
+}, 1000);
+});
+
+
+// div background animate
+$(window).scroll(function () {
+     
+    var x = $(this).scrollTop();
+
+    // freezze div background
+    $('.banner0').css('background-position', '0px ' + x +'px');
+
+    // from left to right
+    $('.banner0').css('background-position', x+'px ' +'0px');
+
+    // from right to left
+    $('.banner0').css('background-position', -x+'px ' +'0px');
+
+    // from bottom to top
+    $('#skills').css('background-position', '0px ' + -x + 'px');
+
+    // move background from top to bottom
+    $('.skills1').css('background-position', '0% ' + parseInt(-x / 1) + 'px' + ', 0% ' + parseInt(-x / 1) + 'px, center top');
+  //alert(x);
+
+  if ( x > 100 ) {
+    $( ".menu" ).addClass( 'menushow' );
+    $( ".menu" ).fadeIn("slow");
+    $( ".menu" ).animate({opacity: 0.75}, 500);
+  } else {
+    $( ".menu" ).removeClass( 'menushow' );
+    $( ".menu" ).animate({opacity: 1}, 500);
+  }
+
+});
+
+
+
+// progres bar animation simple
+$('.bar1').each(function(i) {
+  var width = $(this).data('width');  
+  $(this).animate({'width' : width + '%' }, 900, function(){
+    // Animation complete
+  });  
+});
+
+
+  //Animate skill bars adwanced
+  jQuery('#skills').one('inview', function (event, visible) {
+
+      if (visible == true) {
+          jQuery('.bar').each(function () {   
+              jQuery(this).animate({
+                  width: $(this).attr('data-width')},
+                  {
+                    duration: 5000,
+                    step: function(n){
+                      var txt = $(this).children('span').text();    
+                      txt = $(this).attr('data-txt');
+                      $(this).html( "<span>" + txt + "</span>" + parseInt(n) + "% ");
+                  }
+              });
+          });
+      }
+  });
+
+$('.banner0').click(function(){
+  //alert("dd");
+  var w = $('.banner').width();
+  $('.banner').stop().animate({ backgroundPosition: '+='+w+'px'  }, 1000);
+  //$('.banner').stop().animate({ backgroundPosition: '+=100%'  }, 1000);
+});
+
+
+
+// slide to top of the page
+$('.up').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+
+// menu button show hide
+$(".menubtn").click(function(){
+    $("html, body").animate({scrollTop: 0}, 300);
+       $(".menutop").slideDown( "fast", function() {
+      // Animation complete.
+    });
+});
+
+// slide page to anchor
+$('.menutop b').click(function(){
+    //event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 600);
+    return false;
+});
+
+//$(document.body).animate({    'scrollTop':   $('#anchorName2').offset().top}, 2000);
+
+});
+
+
+
+/* validate email and name */
+function validateText(nameField){
+    var reg = /^[A-Za-z0-9][A-Za-z0-9 -żźćńółęśŻŹĆŃÓŁĘŚ]*$/i;     
+       if (reg.test(nameField.value) == false)
+        {
+            //alert('Podaj Imię');
+            document.example.text.value = "Wpisz wiadomość";
+            document.example.text.setAttribute("style", "color: #ff0000; border-color: #ff0000");
+            return false;
+        }
+        document.example.text.setAttribute("style", "color: #f26621; border-color: #f26621");
+        return true;
+}
+
+function validateName(nameField){
+    var reg = /^[A-Za-z0-9][A-Za-z0-9 -żźćńółęśŻŹĆŃÓŁĘŚ]*$/i;     
+       if (reg.test(nameField.value) == false)
+        {
+            //alert('Podaj Imię');
+            document.example.name.value = "Podaj swoje imię";
+            document.example.name.setAttribute("style", "color: #ff0000; border-color: #ff0000");
+            return false;
+        }
+        document.example.name.setAttribute("style", "color: #f26621; border-color: #f26621");
+        return true;
+}
+
+function validateEmail(emailField){
+        var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+        if (reg.test(emailField.value) == false) 
+        {
+            //alert('Podaj poprawny adres email');
+            document.example.email.value = "Podaj poprawny adres email";
+            document.example.email.setAttribute("style", "color: #ff0000; border-color: #ff0000");
+            return false;
+        }
+        document.example.email.setAttribute("style", "color: #f26621; border-color: #f26621");
+        return true;
+}
+
+
+
+/* scroll page to anchor */
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top-125
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
