5b7b0f37dbe5976710a31435274b0e9a6ae572bd
Fxstar.eu <hello@fxstar.eu>
2017-03-02 10:56:33 +0100

    Create css.php

---
+++ CSS/css.php
@@ -0,0 +1,9 @@
+	<!-- css, icon -->	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">		
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.1.0/css/hover-min.css">
+	
+  <!-- jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>	
+	<script src="main.js"></script>
+	
