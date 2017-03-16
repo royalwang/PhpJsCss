73e7742c0172058f54d299a11a1a6e7035d7dba3
Fxstar.eu <hello@fxstar.eu>
2017-03-16 12:34:26 +0100

    Update emoticon-replace.js

--- emoticonJS/emoticon-replace.js
+++ emoticonJS/emoticon-replace.js
@@ -155,6 +155,13 @@ function preload() {
 
 preload(emoticons);
 
+function is_cached(src) {
+    var image = new Image();
+    image.src = src;
+
+    return image.complete;
+}
+
 function parseURL($string){
 	var __urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 	var __imgRegex = /\.(?:jpe?g|gif|png)$/i;
@@ -169,4 +176,33 @@ function parseURL($string){
             }
         }
     );
+}
+
+function parseYoutubeUriImgToTag($string){
+	var __imgRegex = /\.(?:jpe?g|gif|png)$/i;
+	var __YouRegex = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+	var __urlRegexHttp = /(\b(http|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	var __urlRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+	var exp = __urlRegex;
+	return $string.replace(exp,function(url){ 
+		__imgRegex.lastIndex=0;
+   		// youtube
+		if(__YouRegex.test(url)){			
+			if (url.indexOf('embed') < 0) {
+			    var u = url.match(__YouRegex);
+			    if (u && u[2].length == 11) {			    	
+			        return '<iframe width="300" height="166" src="//www.youtube.com/embed/' + u[2]+ '" frameborder="0" allowfullscreen></iframe>';
+			    } else {
+			        return url;
+			    }
+			}
+        }
+        if(__urlRegexHttp.test(url)){
+        	return '<a href="'+url+'" target="_blank" class="links external">'+url+'</a>'; 
+        }else if(__imgRegex.test(url)){
+            return '<img src="'+url+'" class="links external" />';
+        }else{
+			return '<a href="'+url+'" target="_blank" class="links external">'+url+'</a>'; 
+		}
+	});
 }
