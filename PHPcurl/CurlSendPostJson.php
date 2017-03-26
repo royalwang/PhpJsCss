7d8f77c9f527323db1191447e30890eead8dfde1
Fxstar.eu <hello@fxstar.eu>
2017-03-26 12:44:09 +0200

    Curl send post data Json
    Curl send post data Json

---
+++ PHPcurl/CurlSendPostJson.php
@@ -0,0 +1,24 @@
+<?php
+$url = 'http://localhost/forex/z/curl-req.php';
+
+$data = array("name" => "Heniek", "age" => "125", "rozmiar" => "M");                                                                    
+$data = json_encode($data);                                                                                   
+
+// Send post data format json
+echo CurlSendPost($url,$data);
+
+// send curl post                                        
+function CurlSendPost($url='http://localhost/forex/z/curl-req.php',$datajson){   
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $datajson);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json','Content-Length: ' . strlen($datajson)));
+	return $result = curl_exec($ch);
+}
+?>
+
+<?php
+// curl-req.php
+echo $jsonStr = file_get_contents("php://input");
+?>
