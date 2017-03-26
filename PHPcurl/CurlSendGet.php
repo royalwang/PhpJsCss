38985f16a5bef62584ffa0e8f8c65a4c191f30d0
Fxstar.eu <hello@fxstar.eu>
2017-03-26 12:49:06 +0200

    Php Curl send GET
    Php Curl send GET

---
+++ PHPcurl/CurlSendGet.php
@@ -0,0 +1,17 @@
+<?php
+$url = 'http://localhost/forex/z/curl-req.php';
+
+echo CurlSendPost($url);
+
+function CurlSendGet($url ='http://localhost/forex/z/curl-req.php?item1=value&item2=value2'){
+	$curl = curl_init();	
+	curl_setopt_array($curl, array(
+	    CURLOPT_RETURNTRANSFER => 1,
+	    CURLOPT_URL => $url,
+	    CURLOPT_USERAGENT => 'FxStarBrowser'
+	));	
+	$res = curl_exec($curl);	
+	curl_close($curl);
+	return $res;
+}
+?>
