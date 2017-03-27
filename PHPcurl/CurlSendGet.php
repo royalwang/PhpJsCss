c7d79312a40f60fcd5a63f4bec939a4367c24a3f
Fxstar.eu <hello@fxstar.eu>
2017-03-27 16:46:38 +0200

    Update CurlSendGet.php

--- PHPcurl/CurlSendGet.php
+++ PHPcurl/CurlSendGet.php
@@ -8,6 +8,7 @@ function CurlSendGet($url ='http://localhost/forex/z/curl-req.php?item1=value&it
 	curl_setopt_array($curl, array(
 	    CURLOPT_RETURNTRANSFER => 1,
 	    CURLOPT_URL => $url,
+	    CURLOPT_SSL_VERIFYPEER => false,
 	    CURLOPT_USERAGENT => 'FxStarBrowser'
 	));	
 	$res = curl_exec($curl);	
