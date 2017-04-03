3d2f53212fc14dc094ae9268c4ac570860f1cb4f
Fxstar.eu <hello@fxstar.eu>
2017-04-03 10:16:20 +0200

    Update install.sh

--- roundcube/install.sh
+++ roundcube/install.sh
@@ -12,3 +12,6 @@ extension=php_ldap.dll
 After uploading the files point your browser to  http://url-to-roundcube/installer/ 
 
 ## hmailserver set SSL/TLS in Connection security
+
+## mime types file
+http://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types
