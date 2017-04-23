ece05957ab5226d535efb9bb8c3952bf5bb058c4
Fxstar.eu <hello@fxstar.eu>
2017-04-23 18:14:40 +0200

    Kodowanie znaków z utf8 do Windows1250
    Kodowanie znaków z utf8 do Windows1250

--- ZmianaKodowania/Zmiana-kodowania.php
+++ ZmianaKodowania/Zmiana-kodowania.php
@@ -4,7 +4,14 @@ function correct_encoding($text) {
     $text = iconv($current_encoding, 'UTF-8', $text);
     return $text;
 }
+// kodowanie utf-8 z db plik php
 ini_set('default_charset', 'UTF-8');
 mysql_query("SET NAMES 'utf8'");
+
+// utf-8
+$tocomasz = "Zażółć gęślą jaźń Kórka wodna śąćęążźćłęó ";
 $tocochcesz = iconv('WINDOWS-1250', 'UTF-8', $tocomasz);
+
+echo "Zapisuję do pliku w formacie Windows1250";
+file_put_contents('windows1250.txt', $tocochcesz);
 ?>
