4480acf7b942d6ae99770fc69f4460c6658e59e3
Fxstar.eu <hello@fxstar.eu>
2017-06-19 22:47:20 +0200

    Create default-config.php

---
+++ PDOapi/default-config.php
@@ -0,0 +1,16 @@
+<?php
+/* Config file */
+
+// Mysql database
+$mdatabase = 'sun';
+$muser = 'root';
+$mpass = 'toor';
+$mhost = 'localhost';
+$mport = 3306;
+
+// email settings
+$sendmail_from = 'info@qflash.pl';
+
+// Twój adres email
+$sunemail = 'info@qflash.pl';
+?>
