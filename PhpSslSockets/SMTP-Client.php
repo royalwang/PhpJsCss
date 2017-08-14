c24d5a9a89561fdc2622d8818e3ee53bcd8dd051
Breakermind.com <postmaster@breakermind.com>
2017-08-14 10:59:58 +0200

    Php smtp ssl socket client
    Php smtp ssl socket client

---
+++ PhpSslSockets/SMTP-Client.php
@@ -0,0 +1,29 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$ctx = stream_context_create();
+stream_context_set_option($ctx, 'ssl', 'verify_peer', false);
+stream_context_set_option($ctx, 'ssl', 'verify_peer_name', false);
+try{
+    echo $socket = stream_socket_client('ssl://127.0.0.1:587, $err, $errstr, 60, STREAM_CLIENT_CONNECT, $ctx);
+    if (!$socket) {
+        print "Failed to connect $err $errstr\n";
+        return;
+    }else{
+        // Http
+        // fwrite($socket, "GET / HTTP/1.0\r\nHost: www.example.com\r\nAccept: */*\r\n\r\n");
+        // Smtp
+        echo fread($socket,8192);
+        echo fwrite($socket, "Hello localhost \n");
+        echo fread($socket,8192);
+        echo fwrite($socket, "QUIT \n");
+        echo fread($socket,8192);
+        fclose($socket);    
+    }
+}catch(Exception $e){
+    echo $e;
+}
+die();
+?>
