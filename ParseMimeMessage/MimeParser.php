cf54e8337cdcb37ce229a6fed0675dfd70c520c1
fxstar <postmaster@breakermind.com>
2017-11-04 12:39:08 +0100

    PHP MimeParser - parse mime message
    PHP MimeParser - parse mime message

--- ParseMimeMessage/MimeParser.php
+++ ParseMimeMessage/MimeParser.php
@@ -1,3 +1,4 @@
+// PHP MimeParser - parse mime message
 <?php
 
 /**
@@ -320,112 +321,23 @@ class MimeParser{
 }
 
 $str = file_get_contents('mime-mixed-related-alternative.eml');
+
 // MimeParser
 $m = new MimeParser($str);
+
 // Emails
 print_r($m->mTo);
 print_r($m->mFrom);
 print_r($m->mBcc);
 print_r($m->mCc);
+
 // Message
 echo $m->mSubject;
 echo $m->mHtml;
 echo $m->mText;
 print_r($m->mInlineList);
+
 // files
 echo "<pre>";
 print_r($m->mFiles);
 
-die();
-
-// files
-foreach ($parts as $key => $value) {
-	$m->setMimePart($value);
-	echo $m->getContentType();
-	// get file content
-	$m->isFile(1);
-	$m->getFileName();
-	$m->getInlineID();
-	$m->getFileEncoding();	
-}
-
-
-
-preg_match_all('/((?<=(Content-Type: multipart\/mixed; boundary="))(.*)?(?=(")))|((?<=(Content-Type: multipart\/related; boundary="))(.*)?(?=(")))|((?<=(Content-Type: multipart\/alternative; boundary="))(.*)?(?=(")))/', $str, $boundary);
-
-// echo "<pre>";
-// print_r($boundary);
-
-$AllPartsUnique = "";
-$j=0;
-
-foreach ($boundary[0] as $key => $v) {
-  if($key >= 0){
-    // echo "\n\n\nBoundary " . $v . "\r\n";
-
-    // cut boundary content
-    preg_match_all('/(?<=(--'.$v.'))(| |.*|[\s\S]+|\<|\>|\.|\r|\n|\0|@|\w+)?(?=(--'.$v.'--))/', $str, $part);
-    print_r($part);
-    $bname = $v;
-
-    foreach ($part[0] as $v) {
-        // echo "PART " . $bname . " " . $v . "\r\n";
-        $parts = explode("--".$bname, $v);
-
-        echo "<pre>";
-        foreach ($parts as $v) {
-            // echo "\r\nSINGLE PART " . $v . "\r\n";     
-            // $AllPartsUnique[$j] = $v;
-            // with html visible on page
-            $AllPartsUnique[$j] = htmlentities($v);     
-            $j++;  
-        }
-    }
-  }
-}
-// echo "<pre>";
-// print_r($AllPartsUnique);
-
-foreach($AllPartsUnique as $key => $one) {
-  foreach ($boundary[0] as $find) {
-    if(strpos($one, $find) !== false){
-        unset($AllPartsUnique[$key]); 
-    }
-  }    
-}
-
-// echo "<pre>";
-// print_r($AllPartsUnique);
-
-foreach ($AllPartsUnique as $v) {
-	$e =  explode("\r\n\r\n", $v);
-	// echo $v;
-	print_r($e);
-
-	$m = new mimePart($e);
-	// get file content
-	$m->isFile(1);
-	$m->getFileName();
-	$m->getInlineID();
-	$m->getFileEncoding();
-	echo $m->getContentType();
-	
-}
-
-
-preg_match_all('/(?<=((\n)To: )|(^To: ))(.*)+?(?=())/', $str, $to);
-// preg_match_all('/To:(.*)/', $str, $to);
-echo "To " . htmlentities($to[0][0]);
-
-preg_match_all('/(?<=((\n)From: )|(^From: ))(.*)+?(?=())/', $str, $from);
-// preg_match_all('/From:(.*)/', $str, $to);
-echo "From " . htmlentities($to[0][0]);
-
-preg_match_all('/(?<=((\n)Subject: )|(^Subject: ))(.*)+?(?=())/', $str, $subject);
-echo "Subject " . $subject[0][0];
-    
-// end script
-die();
-
-
-?>
