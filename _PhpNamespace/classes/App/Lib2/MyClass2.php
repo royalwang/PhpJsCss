b2cdcef02394fea0900f6d8d54ea38547e6f09d7
Fxstar <16883796+fxstar@users.noreply.github.com>
2018-03-08 11:41:33 +0100

    Update MyClass2.php

--- _PhpNamespace/classes/App/Lib2/MyClass2.php
+++ _PhpNamespace/classes/App/Lib2/MyClass2.php
@@ -2,10 +2,49 @@
 namespace App\Lib2;
 
 echo "<br> Current namespace " . __NAMESPACE__ . "<br>";
+/**
+* 
+*/
 
-class MyClass2 {
-	public function WhoAmI() {
+class MyClass2
+{	
+	private $hTxtPriv = "Hello text private";
+	public $hTxtPub = "Hello text public";
+	protected $hTxtPro = "Hello text protected";
+
+	public function WhoAmI(){
 		return "Second class " . __METHOD__ . "<br>";
+	}	
+
+	protected function HelloPro($txt = "HOHoHo"){
+		echo "<br>";
+		echo $this->hTxtPub;
+		echo $this->hTxtPriv;
+		echo $this->hTxtPro;
+		echo "<br>";
+		return $txt;
+	}
+
+	private function HelloPriv($txt = "HOHoHo"){
+		echo "<br>";
+		echo $this->hTxtPub;
+		echo $this->hTxtPriv;
+		echo $this->hTxtPro;
+		echo "<br>";
+		return $txt;
+	}
+
+	public function HelloPub($txt = "HOHoHo"){
+		$this->HelloPriv();
+		$this->HelloPro();
+		echo "<br>";		
+		echo $this->hTxtPub;
+		echo $this->hTxtPriv;
+		echo $this->hTxtPro;
+		echo "<br>";
+		return $txt;
 	}
 }
+
+
 ?>
