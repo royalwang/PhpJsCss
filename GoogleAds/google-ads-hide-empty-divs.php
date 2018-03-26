9c09b0d7c59a8f8e594899f48e581112322fd14e
Fxstar <16883796+fxstar@users.noreply.github.com>
2018-03-26 11:05:30 +0200

    Google ads close empty divs
    Google ads close empty divs

---
+++ GoogleAds/google-ads-hide-empty-divs.php
@@ -0,0 +1,11 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- kupfure.pl -->
+<ins class="adsbygoogle" style="display:block"
+    data-ad-client="ca-pub-6372942657343487"
+    data-ad-slot="8312140807"
+    data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({
+    // googletag.pubads().collapseEmptyDivs();
+}).setCollapseEmptyDiv(true);
+</script>
