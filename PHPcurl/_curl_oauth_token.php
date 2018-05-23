db5b3c4aef64784e9b6a77464bb606d1da809018
Fxstar <16883796+fxstar@users.noreply.github.com>
2018-05-23 10:15:20 +0200

    Curl with header token oauth
    Curl with header token oauth

---
+++ PHPcurl/_curl_oauth_token.php
@@ -0,0 +1,37 @@
+<?php
+
+function sms_send($params, $token)
+{
+
+    static $content;    
+    $url = 'https://api.smsapi.pl/sms.do';
+
+    $c = curl_init();
+    curl_setopt($c, CURLOPT_URL, $url);
+    curl_setopt($c, CURLOPT_POST, true);
+    curl_setopt($c, CURLOPT_POSTFIELDS, $params);
+    curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($c, CURLOPT_HTTPHEADER, array(
+        "Authorization: Bearer $token"
+    ));
+
+    $content = curl_exec($c);
+    $http_status = curl_getinfo($c, CURLINFO_HTTP_CODE);
+    if ($http_status != 200) {        
+        sms_send($params, $token);
+    }
+    curl_close($c);
+    return $content;
+}
+
+$token = "wygenerowany_token"; //https://ssl.smsapi.com/webapp#/oauth/manage
+
+$params = array(
+    'to' => '900000000', //numery odbiorców rozdzielone przecinkami
+    'from' => 'Info', //pole nadawcy
+    'message' => "Hello world!" //treść wiadomości
+);
+
+echo sms_send($params, $token);
+
+?>
